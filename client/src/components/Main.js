749bc933266e17332a6d57bbb766368d4fc191d6
Jess <jsscthomp@gmail.com>
2017-10-03 17:45:21 -0400

    main.js nav bar added

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -7,6 +7,27 @@ export class Main extends React.Component {
             <div className="main-container">
                 <div className="container">
 
+                    <nav className="navbar navbar-default" role="navigation">
+                        <div className="container-fluid">
+                            <div className="navbar-header">
+                                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                                    <span className="sr-only">Home</span>
+                                    <span className="icon-bar"></span>
+                                    <span className="icon-bar"></span>
+                                    <span className="icon-bar"></span>
+                                </button>
+                                <a className="navbar-brand" href="#">NYT React</a>
+                            </div>
+
+                            <div className="collapse navbar-collapse navbar-ex1-collapse">
+                                <ul className="nav navbar-nav navbar-right">
+                                    <li><a href="#/search">Search</a></li>
+                                    <li><a href="#/saved">Saved</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+
                     <div className="jumbotron">
                         <h1 className="title">New York Times Article Scrubber</h1>
                     </div>
