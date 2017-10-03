dad07084c1104e676756972ab75f29cb7f47b5f2
Jess <jsscthomp@gmail.com>
2017-10-03 14:52:42 -0400

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-import './App.css';
 import ReactDom from 'react-dom';
 import Router from 'react-router';
 import routes from '../routes';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+ReactDOM.render(
 
-export default App;
+  <Router>{routes}</Router>,
+  document.getElementById('root')
+
+);
