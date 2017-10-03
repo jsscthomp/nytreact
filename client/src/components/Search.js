01388f9e5980f9d9ede70ff97074c5b5945d77b2
Jess <jsscthomp@gmail.com>
2017-10-02 21:52:47 -0400

    search.js finished

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,6 +1,8 @@
 const React = require('react');
 const Router = require('react-router');
 const API = require('../utils/API');
+const Query = require('./Search/Query');
+const Results = require('./Search/Results');
 
 export class Search extends React.Component {
 
@@ -13,5 +15,38 @@ export class Search extends React.Component {
         }
     },
 
-    
-}
+    componentDidUpdate: function(prevProps, prevState) {
+
+        if (this.state.queryTerm != "" && (prevState.queryTerm != this.state.queryTerm || prevState.startYear != this.state.startYear || prevState.endYear != this.state.endYear)) {
+            API.runQuery(this.state.queryTerm, this.state.startYear, this.state.endYear)
+            .then(function(data) {
+                if (data != this.state.results) {
+                    this.setState({
+                        results: data
+                    })
+                }
+            }.bind(this))
+        }
+    },
+
+    setQuery: function(newQuery, newStart, newEnd) {
+        this.setState({
+            queryTerm: newQuery,
+            startYear: newStart,
+            endYear: newEnd
+        })
+    },
+
+    render() {
+        return(
+            <div className="main-container">
+
+                <Query updateSearch={this.setQuery} />
+                <Results results={this.state.results} />
+
+            </div>
+        )
+    }
+
+
+};
