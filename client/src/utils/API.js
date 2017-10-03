670467e509cebc975d3a71e0c63bc1d02da56a51
Jess <jsscthomp@gmail.com>
2017-10-02 23:03:44 -0400

    working on route.js and updated API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,7 @@ const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 const APIKEY = "&apikey=e92e49d7263d4506ac6e8571b2fdc0dc";
 
 const API = {
-
+    // set up call to get articles from API
     runQuery: function(term, start, end) {
 
         var term = term.trim();
@@ -17,13 +17,12 @@ const API = {
                 'start_date': start,
                 'end_date': end
             }
-        })
-
-        .then(function(results) {
+        }).then(function(results) {
             return results.data.response;
         });
     },
 
+    // get saved articles from db
     getSaved: function() {
         return axios.get('/api/saved')
             .then(function(results) {
@@ -31,6 +30,7 @@ const API = {
             })
     },
 
+    // post saved articles to db
     postSaved: function(title, date, url) {
         var newArticle = {title: title, date: date, url: url};
         return axios.post('/api/saved', newArticle)
@@ -39,6 +39,7 @@ const API = {
             })
     },
 
+    // delete article from db
     deleteSaved: function(title, date, url) {
         return axios.delete('/api/saved', {
             params: {
