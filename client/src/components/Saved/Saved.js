075bcf087187e1ec625970fd4edb0cf7b61b8033
Jess <jsscthomp@gmail.com>
2017-10-02 22:13:08 -0400

    working on saved.js

--- client/src/components/Saved/Saved.js
+++ client/src/components/Saved/Saved.js
@@ -2,3 +2,63 @@ const React = require('react');
 const Router = require('react-router');
 const API = require('../utils/API');
 
+export class Main extends React.Component {
+
+    initialState: function() {
+        return {
+            savedArticles: ""
+        }
+    },
+
+    componentDidMount: function() {
+        API.getSaved()
+            .then(function(articleData) {
+                this.setState({
+                    savedArticles: articleData.data
+                });
+            }.bind(this))
+    },
+
+    handleClick: function(item, event) {
+        API.deleteSaved(item.title, item.date, item.url) 
+            .then(function(data) {
+            
+            API.getSaved()
+                .then(function(articleData) {
+                    this.setState({
+                        savedArticles: articleData.data
+                    });
+                }.bind(this))
+            }.bind(this))
+    },
+
+    render() {
+        if (!this.state.savedArticles === "") {
+            return(
+                <li className="list-group-item">
+                    <h3>
+                        <span><em>Whoops! You don't have any saved articles yet!</em></span>
+                    </h3>
+                </li>
+            )
+        } else {
+
+            var articles = this.state.savedArticles.map(function(article, index) {
+                return(
+                    <div key={index}>
+                        <li className="list-group-item">
+                            <h3>
+                                <span><em>{article.title}</em></span>
+                                <span className="btn-group pull-right">
+                                    <button className="btn btn-default"><a href={article.url} target="_blank">View Article</a></button>
+                                    <button className="btn btn-danger" onClick={this.handleClick.bind(this.article)}></button>
+                                </span>
+                            </h3>
+                            <p>Date Published: {article.date}</p>
+                        </li>
+                    </div>
+                )
+            })
+        }
+    }
+}
