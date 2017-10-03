d50ebfd1e7aab87444e63836f691d9a751b4e7e6
Jess <jsscthomp@gmail.com>
2017-10-03 14:38:01 -0400

    updated routes.js

---
+++ routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Router from 'react-router';
+const Route = Router.Route;
+const IndexRoute = Router.IndexRoute;
+import Main from '../client/src/components/Main';
+import Search from '../client/src/components/Search';
+import Saved from '../client/src/components/Saved';
+
+module exports = (
+
+    <Route path='/' component={Main}>
+
+        <Route path='Search' component={Search} />
+        <Route path='Saved' component={Saved} />
+
+        <IndexRoute component={Search} />
+
+    </Route>
+);
