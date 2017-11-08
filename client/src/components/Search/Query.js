bbc1f9066340f8f0994c55c7e4462739ea052c27
Jess <jsscthomp@gmail.com>
2017-11-07 21:25:44 -0500

    updated query component

--- client/src/components/Search/Query.js
+++ client/src/components/Search/Query.js
@@ -1,7 +1,9 @@
-const React = require('react');
+// include react dependency
+import React, { Component } from 'react';
 
+// query component declaration
 export class Query extends React.Component {
-
+// set initial variables
     constructor() {
         super();
         this.state = {
@@ -11,17 +13,20 @@ export class Query extends React.Component {
         }
     }
 
+    // handle changes from textbox
     handleChange(event) {
         var newState = {};
         newState[event.target.id] = event.target.value;
         this.setState(newState);
     }
 
-    handleSubmit() {
+    // handle what happens to the search terms. sends to parent search
+    handleSubmit(event) {
+        event.preventDefault();
         this.props.updateSearch(this.state.search, this.state.start, this.state.end);
-        return false;
     }
 
+    // render the query component
     render() {
         return(
             <div className="main-container">
@@ -37,32 +42,55 @@ export class Query extends React.Component {
 
                             <div className="panel-body">
 
+                                {/* associate text-box input with state values */}
                                 <form>
 
                                     <div className="form-group">
                                         <h3 className="topic">Topic</h3>
-                                        <input type="text" value={this.state.value} className="form-control" id="search" onChange= {this.handleChange} required />
+                                        <input 
+                                            type="text" 
+                                            value={this.state.value} 
+                                            className="form-control" 
+                                            id="search" 
+                                            onChange= {this.handleChange} 
+                                            required 
+                                            />
 
                                         <h3 className="start">Start Year (Required)</h3>
-                                        <input value= {this.state.value} className="form-control" id="start" onChange= {this.handleChange} required />
+                                        <input 
+                                            value= {this.state.value} 
+                                            className="form-control" 
+                                            id="start" 
+                                            onChange= {this.handleChange} 
+                                            required 
+                                            />
 
                                         <h3 className="end">End Year (Required)</h3>
-                                        <input value={this.state.value} className="form-control" id="end" onChange={this.handleChange} required />
+                                        <input 
+                                            value={this.state.value} 
+                                            className="form-control" 
+                                            id="end" 
+                                            onChange={this.handleChange} 
+                                            required 
+                                            />
                                     </div>
-
+                                    {/* create onclick event that triggers handleSubmit method */}
                                     <div className="pull-right">
-                                        <button type="button" className="btn btn-secondary" onClick={this.handleSubmit}>Submit</button>
+                                        <button 
+                                            type="button" 
+                                            className="btn btn-secondary" 
+                                            onClick={this.handleSubmit}
+                                            >
+                                            <h4>Submit</h4>
+                                        </button>
                                     </div>
-
                                 </form>
 
                             </div>
-
                         </div>
 
                     </div>
                 </div>
-
             </div>
         )
     }
